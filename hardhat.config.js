e8d70eda9f24145fabe0a052bbeafbfb79bcad86
Johan B George <johanb25george@gmail.com>
2025-10-20 17:53:33 +0530

    contracts and scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,13 @@
-// require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
-
+require("@nomicfoundation/hardhat-toolbox");
+//require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
+        version: "0.8.17",
         settings: {
           evmVersion: "istanbul",
           optimizer: {
@@ -20,7 +21,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://rpc.ankr.com/eth",
+        url: 'https://eth-mainnet.g.alchemy.com/v2/TVAtmn6ES2BGwWHYa3ZG9',
       },
     },
   },
