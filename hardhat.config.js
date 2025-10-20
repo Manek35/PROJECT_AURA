4d5abca4adccefd4f3ceb64f11fdd9a6e8e68e1a
Johan B George <johanb25george@gmail.com>
2025-10-20 17:57:36 +0530

    api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: 'https://eth-mainnet.g.alchemy.com/v2/TVAtmn6ES2BGwWHYa3ZG9',
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       },
     },
   },
